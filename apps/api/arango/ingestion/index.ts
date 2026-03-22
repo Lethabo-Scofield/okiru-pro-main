1385023a16e5244e081c168557c4ef7c85e19cb7
ScoField <scofieldx911@gmail.com>
2026-03-22 11:56:04 +0200

    Removed Outdated Frontend

--- apps/api/arango/ingestion/index.ts
+++
@@ -1,6 +0,0 @@
-export {
-  ingestToolkitTemplate,
-  ingestToolkitFromBuffer,
-  ingestAllToolkits,
-} from './templateIngester.js';
-export type { IngestionResult, BulkIngestionResult } from './templateIngester.js';
